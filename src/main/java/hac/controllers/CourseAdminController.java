951793447ab3bbd63fb5a1690e8c5ab0436c6486
Miriam Binyamines <89273650+MiriamBin@users.noreply.github.com>
2023-06-09 20:22:14 +0300

    added delete & edit buttons - need to make it work table

--- src/main/java/hac/controllers/CourseAdminController.java
+++ src/main/java/hac/controllers/CourseAdminController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CourseAdminController {
@@ -24,6 +26,7 @@ public class CourseAdminController {
 
     @PostMapping("/addCourse")
     public String addCourse(@Valid Course course, BindingResult result, Model model) {
+        System.out.println("addCourse: " + course);
         if (result.hasErrors()) {
             return "add-course";
         }
@@ -33,4 +36,54 @@ public class CourseAdminController {
         return "add-course";
     }
 
+    @GetMapping("/editCourse/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Course course = courseRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid course Id:" + id));
+        model.addAttribute("course", course);
+        return "edit-coureses";
+    }
+
+    @PostMapping("/updateCourse/{id}")
+    public String updateCourse(@PathVariable("id") long id, @Valid Course course,
+                               BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            course.setId(id);
+            return "edit-coureses";
+        }
+
+        courseRepo.save(course);
+        model.addAttribute("courses", courseRepo.findAll());
+        return "edit-coureses";
+    }
+
+    @PostMapping("/deleteCourse")
+    public String deleteCourse(@RequestParam("id") long id, Model model) {
+
+        System.out.println("deleteUser: 1111111");
+        // we throw an exception if the user is not found
+        // since we don't catch the exception here, it will fall back on an error page (see below)
+        Course user = courseRepo
+                .findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("Invalid course Id:" + id)
+                );
+        courseRepo.delete(user);
+        model.addAttribute("courses", courseRepo.findAll());
+        return "add-course";
+    }
+
+//    @GetMapping("/delete/{id}")
+//    public String deleteUserGet(@PathVariable long id) {
+//
+//        System.out.println("deleteUser: 222222");
+//        return "redirect:add-course";
+//    }
+//
+//    @GetMapping("/delete")
+//    public String deleteUserGetNoParams() {
+//        System.out.println("deleteUser: 333333");
+//
+//        return "redirect:add-course";
+//    }
 }
